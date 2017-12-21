1c8f0a5c04474aab69a6958cd74a901368539d89
Artur Sokolov <artur.sokolov@tochka.com>
2017-12-21 22:41:32 +0500

    MarkdownTagHelper

---
+++ core/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using CommonMark;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace Tochka.TagHelpers
+{
+    [HtmlTargetElement("markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        public ModelExpression Value { get; set; }
+
+        public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagMode = TagMode.SelfClosing;
+            output.TagName = null;
+
+            string html = CommonMarkConverter.Convert(await GetContent(output));
+            output.Content.SetHtmlContent(html ?? "");
+        }
+
+        private async Task<string> GetContent(TagHelperOutput output)
+        {
+            if (Value == null)
+                return (await output.GetChildContentAsync()).GetContent();
+
+            return Value.Model?.ToString();
+        }
+    }
+}
